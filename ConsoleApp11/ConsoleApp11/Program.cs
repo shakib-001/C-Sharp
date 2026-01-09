59edeb2ccb258c4a6ab374bdd1d3f4fcd47ef8ea
shakib-001 <138766692+shakib-001@users.noreply.github.com>
2026-01-10 00:34:57 +0600

    Add files via upload

---
+++ ConsoleApp11/ConsoleApp11/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+// Payment Contract
+public interface IPayment
+{
+    string ProviderName { get; }
+    void Pay(double amount);
+}
+// Cash Payment
+public class CashPayment : IPayment
+{
+    public string ProviderName
+    {
+        get { return "Cash Payment"; }
+    }
+    public void Pay(double amount)
+    {
+        Console.WriteLine($"Paid {amount} taka using Cash.");
+    }
+}
+// Card Payment
+public class CardPayment : IPayment
+{
+    public string ProviderName
+    {
+        get { return "Card Payment"; }
+    }
+    public void Pay(double amount)
+    {
+        Console.WriteLine($"Paid {amount} taka using Card.");
+    }
+}
+// Main Program
+class Program
+{
+    static void Main()
+    {
+        List<IPayment> payments = new List<IPayment>
+        {
+            new CashPayment(),
+            new CardPayment()
+        };
+        foreach (IPayment payment in payments)
+        {
+            payment.Pay(500);
+            Console.WriteLine(payment.ProviderName);
+            Console.WriteLine("-------------------");
+        }
+        Console.ReadLine();
+    }
+}
